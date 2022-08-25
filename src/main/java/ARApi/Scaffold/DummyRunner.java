176173fa8f1294a0102c40037833e7be3693a470
tobi97h <tobi_h@outlook.com>
2022-08-25 19:30:35 +0200

    dummy runner and version
    new version

---
+++ src/main/java/ARApi/Scaffold/DummyRunner.java
@@ -0,0 +1,8 @@
+package ARApi.Scaffold;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyRunner {
+	public static void main(String[] args) {	}
+}
