c5a1d31503d115311312f3ab884565b8adef22d0
tobi97h <tobi_h@outlook.com>
2022-08-28 00:49:15 +0200

    swagger enable set auth token

---
+++ src/main/java/ARApi/Scaffold/SwaggerConf.java
@@ -0,0 +1,38 @@
+package ARApi.Scaffold;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class SwaggerConf {
+
+    @Bean
+    public Docket api() {
+
+        return new Docket(DocumentationType.OAS_30)
+                .securitySchemes(Collections.singletonList(HttpAuthenticationScheme.JWT_BEARER_BUILDER
+                        .name("JWT")
+                        .build()))
+                .securityContexts(Collections.singletonList(SecurityContext.builder()
+                        .securityReferences(Collections.singletonList(SecurityReference.builder()
+                                .scopes(new AuthorizationScope[0])
+                                .reference("JWT")
+                                .build()))
+                        .operationSelector(o -> o.requestMappingPattern().matches("/.*"))
+                        .build()))
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
