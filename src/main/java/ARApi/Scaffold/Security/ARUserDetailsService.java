4de1693d0670566522466deb9edf06b271dd46f5
tobi97h <tobi_h@outlook.com>
2022-09-04 19:46:09 +0200

    add registration origin

--- src/main/java/ARApi/Scaffold/Security/ARUserDetailsService.java
+++ src/main/java/ARApi/Scaffold/Security/ARUserDetailsService.java
@@ -1,20 +1,38 @@
 package ARApi.Scaffold.Security;
 
+import ARApi.Scaffold.Constants;
+import ARApi.Scaffold.Database.Entities.User;
 import ARApi.Scaffold.Database.Repos.UserRepository;
+import ARApi.Scaffold.Shared.Enums.RegistrationOrigin;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import liquibase.repackaged.org.apache.commons.collections4.Get;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
 @Service
-public class ARUserDetailsService implements UserDetailsService {
+public class ARUserDetailsService extends DefaultOAuth2UserService implements UserDetailsService {
 
     UserRepository userRepository;
 
+    final Environment environment;
+
     @Autowired
-    public ARUserDetailsService(UserRepository userRepository) {
+    public ARUserDetailsService(UserRepository userRepository, Environment environment) {
         this.userRepository = userRepository;
+        this.environment = environment;
     }
 
     @Override
@@ -27,5 +45,21 @@ public class ARUserDetailsService implements UserDetailsService {
         }
 
         throw new UsernameNotFoundException(username);
+    }
+
+
+    public String GetJwtForUserPricipal(User user){
+        var dbUser = userRepository.findByEmail(user.getUsername()).orElseGet(ARApi.Scaffold.Database.Entities.User::new);
+
+        var secret  = environment.getProperty(Constants.ENV_VAR_JWT_SECRET);
+
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+
+        return JWT.create()
+                .withIssuer("auth0")
+                .withSubject(dbUser.email)
+                .withClaim("uuid", dbUser.uuid.toString())
+                .withExpiresAt(Instant.now().plus(30, ChronoUnit.MINUTES))
+                .sign(algorithm);
     }
 }
