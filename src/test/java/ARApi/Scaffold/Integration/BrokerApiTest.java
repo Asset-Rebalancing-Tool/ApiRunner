39c4fd16720ba29ecc498af9a6decd6838b5ef76
tobi97h <tobi_h@outlook.com>
2022-09-16 20:11:16 +0200

    example test

---
+++ src/test/java/ARApi/Scaffold/Integration/BrokerApiTest.java
@@ -0,0 +1,15 @@
+package ARApi.Scaffold.Integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@AutoConfigureWebTestClient
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BrokerApiTest {
+
+    @Test
+    public void Test(){
+
+    }
+}
