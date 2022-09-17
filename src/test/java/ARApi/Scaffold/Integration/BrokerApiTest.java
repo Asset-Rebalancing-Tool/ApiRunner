c7e7931104d507a902f51fbec41c422eb0076291
Marc Klose <pmk.00170015@protonmail.com>
2022-09-17 09:49:05 +0200

    Requests until login

--- src/test/java/ARApi/Scaffold/Integration/BrokerApiTest.java
+++ src/test/java/ARApi/Scaffold/Integration/BrokerApiTest.java
@@ -4,6 +4,7 @@ import org.asynchttpclient.uri.Uri;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
@@ -14,6 +15,9 @@ import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @AutoConfigureWebTestClient
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -24,28 +28,35 @@ public class BrokerApiTest {
 
         var client = HttpClient.newHttpClient();
         // First request - get redirect url
-        HttpRequest request = HttpRequest.newBuilder()
+        HttpRequest requestExtendedLogin = HttpRequest.newBuilder()
                 .uri(new URI("https://de.scalable.capital/cockpit/extended-login"))
                 .GET()
                 .build();
-        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        String redirectURI = response.headers().firstValue("location").orElseThrow();
+        var responseExtendedLogin = client.send(requestExtendedLogin, HttpResponse.BodyHandlers.ofString());
+        String redirectURI = responseExtendedLogin.headers().firstValue("location").orElseThrow();
         // Second request - redirect
-        HttpRequest request2 = HttpRequest.newBuilder()
+        HttpRequest requestAuthorize = HttpRequest.newBuilder()
                 .uri(new URI(redirectURI))
                 .GET()
                 .build();
-        var response1 = client.send(request2, HttpResponse.BodyHandlers.ofString());
-
-        var params = UriComponentsBuilder.fromUriString("https://de.scalable.capital" + response1.headers().firstValue("location").toString())
-                    .build().getQueryParams();
+        var responseAuthorize = client.send(requestAuthorize, HttpResponse.BodyHandlers.ofString());
+        var params = UriComponentsBuilder.fromUriString("https://de.scalable.capital" + responseAuthorize.headers().firstValue("location").orElseThrow())
+                    .build()
+                    .getQueryParams();
         var state = params.getFirst("state");
         var formattedString = String.format("https://secure.scalable.capital/u/login?state=%s&ui_locales=%s", state, params.getFirst("ui_locales"));
         // First post request
         HttpRequest requestPost = HttpRequest.newBuilder()
                 .headers("Content-Type", "application/x-www-form-urlencoded")
-                .uri(URI.create(String.format("https://secure.scalable.capital/u/login?state=%s&ui_locales=%s", state, params.getFirst("ui_locales"))))
-                .POST(HttpRequest.BodyPublishers.ofString(""))
+                .uri(URI.create(formattedString))
+                .POST(getParamsUrlEncoded(params))
                 .build();
+    }
+    private HttpRequest.BodyPublisher getParamsUrlEncoded(MultiValueMap<String, String> parameters) {
+        String urlEncoded = parameters.entrySet()
+                .stream()
+                .map(e -> e.getKey() + "=" + URLEncoder.encode(e.getValue().get(0), StandardCharsets.UTF_8))
+                .collect(Collectors.joining("&"));
+        return HttpRequest.BodyPublishers.ofString(urlEncoded);
     }
 }
